0cb9ba90909fecaed67e5606e9df327ce49eebb9
swaraj Chaudhari <69323131+swarajpatil11@users.noreply.github.com>
2025-07-16 11:01:49 +0530

    Update Myreport.java

--- src/main/java/com/utilities/Myreport.java
+++ src/main/java/com/utilities/Myreport.java
@@ -12,7 +12,7 @@ public class Myreport {
 	public static ExtentSparkReporter spark;
 
 	public static ExtentReports Setup() {
-		String path = "test-output/reports.html"; 
+		String path = "test-output/extentReport/reports.html"; 
          	extent = new ExtentReports();
 		spark = new ExtentSparkReporter(path);
 		extent.attachReporter(spark);
